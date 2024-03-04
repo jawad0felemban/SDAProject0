6ebcf22c494be99cb0766d1461d231a2912d56cb
jawad0felemban <JMfelemban@gmail.com>
2024-03-04 14:35:56 +0300

    fix bullets

---
+++ Assets/DestroyMyEnemy.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Diagnostics.Tracing;
+using UnityEngine.Video;
+
+public class DestroyMyEnemy : MonoBehaviour
+{
+    private Rigidbody m_VsObject;
+    //private Pausebutton m_Text;
+    //private MyKillScore m_MyKillCounter;
+
+    void Start()
+    {
+        m_VsObject = GetComponent<Rigidbody>();
+        //m_MyKillCounter = FindAnyObjectByType<MyKillScore>();
+
+
+    }
+
+
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //m_MyKillCounter.IncreaseCounter();
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag == "PlayerWeapon")
+        {
+            Destroy(gameObject);
+        }
+
+
+
+    }
+}
